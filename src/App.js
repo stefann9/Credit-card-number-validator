05a7e14d479f3ef6399c5bea2100d038eff6a6c8
stef <stefan.manea198@gmail.com>
2022-10-10 12:08:40 +0300

    add class component App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(state) {
+    super(state);
+    this.state = {
+      cardNumsArr: [],
+    };
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Card number validator : {this.state.cardNumsArr}</h1>
+        <form action="#">
+          <input type='tel'/>
+        </form>
+        <h1>Display Validator</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
